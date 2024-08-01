2f73395787e20aace25d6a4b2d98fb8ae26d46da
Leroy <ntruderleroy@gmail.com>
2024-08-01 06:43:29 +0300

    minor fixes

--- src/Booklist.jsx
+++ src/Booklist.jsx
@@ -1,42 +1,42 @@
 import React, { useState, useEffect } from "react";
 
-const Booklist = () => {
-  const [book, setBooks] = useState([]);
-  const [loading, setLoading] = useState([true]);
-  const [error, setError] = useState([null]);
+const BooksList = () => {
+	const [books, setBooks] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(null);
 
-  useEffect(() => {
-    const fetchBooks = async () => {
-      try {
-        const response = await fetch("http://localhost:5000/");
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        const data = await response.json();
-        setBooks(data);
-        setLoading(false);
-      } catch (error) {
-        setError(error);
-        setLoading(false);
-      }
-    };
+	useEffect(() => {
+		const fetchBooks = async () => {
+			try {
+				const response = await fetch("http://localhost:3001/books");
+				if (!response.ok) {
+					throw new Error("Network response was not ok");
+				}
+				const data = await response.json();
+				setBooks(data);
+				setLoading(false);
+			} catch (error) {
+				setError(error);
+				setLoading(false);
+			}
+		};
 
-    fetchBooks;
-  }, []);
+		fetchBooks();
+	}, []);
 
-  if (loading) return <div> Loading ...</div>;
-  if (error) return <div> Error fetching data</div>;
+	if (loading) return <div>Loading...</div>;
+	if (error) return <div>Error fetching data</div>;
 
-  return (
-    <div className="book__wrapper">
-      {book.map((book) => (
-        <div key={book.id} className="book-item">
-          <h2>{book.title}</h2>
-          <p> Number of Username: {book.Username.length}</p>
-        </div>
-      ))}
-    </div>
-  );
+	return (
+		<div className="books__wrapper">
+			{books.map((book) => (
+				<div key={book.id} className="book-item">
+					<h2>{book.title}</h2>
+					<p>Number of Assignees: {book.Username.length}</p>
+				</div>
+			))}
+		</div>
+	);
 };
 
-export default Booklist;
+export default BooksList;
